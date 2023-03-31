3df5b95bdacda913365c0c570192fa3671d3d6c4
yinlaenyo <120325015+yinlaenyo@users.noreply.github.com>
2023-03-31 16:39:14 +0630

    Add files via upload

---
+++ assignment9.c
@@ -0,0 +1,23 @@
+//
+// Created by asus on 3/30/2023.
+//
+
+
+
+#include "stdio.h"
+#include "assignment9.h"
+
+int main(){
+    space_counter();
+    loadingAllDataFromFile();
+    printingAllData();
+    welcome();
+
+    //get_time();
+
+
+
+//    integer_to_char(1000);
+
+    return 0;
+}
